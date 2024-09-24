fc6b98694a24d6da80c945e0aa8656b83a1f394b
Алексей <ix.fenix@mail.ru>
2024-09-24 21:36:41 +0300

    feat: enemy death particles effect

--- SpaceInvaders/Assets/_Source/GameAssembly/EnemySystem/Enemy.cs
+++ SpaceInvaders/Assets/_Source/GameAssembly/EnemySystem/Enemy.cs
@@ -4,6 +4,19 @@ namespace EnemySystem
 {
 	public class Enemy : MonoBehaviour
 	{
-		
+		[SerializeField] private ParticleSystem destroyingParticles;
+		[SerializeField] private GameObject ammoPrefab;
+
+		public void Die()
+		{
+			destroyingParticles.Play();
+			
+			gameObject.SetActive(false);
+		}
+
+		public void Shoot()
+		{
+			
+		}
 	}
 }
