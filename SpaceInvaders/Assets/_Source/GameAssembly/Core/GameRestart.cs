2c23dcc95741f6838cabbd5fed78b62ea93acb89
Алексей <ix.fenix@mail.ru>
2024-09-25 19:56:27 +0300

    feat: game restart

---
+++ SpaceInvaders/Assets/_Source/GameAssembly/Core/GameRestart.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Core
+{
+	public class GameRestart : MonoBehaviour
+	{
+		private void Update()
+		{
+			if(Input.GetKeyDown(KeyCode.R))
+				RestartGame();
+		}
+
+		private void RestartGame()
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+}
